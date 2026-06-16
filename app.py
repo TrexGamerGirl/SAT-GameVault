78a9634e5deef0478f81e2be6e2171fcfbcf66ad
TrexGamerGirl <138319@student.bmg.vic.edu.au>
2026-06-17 09:59:43 +1000

    Created structure and blank window for the program in pygame

---
+++ app.py
@@ -0,0 +1,35 @@
+import pygame
+from pathlib import Path
+
+#import repos
+#import screens
+
+class App:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((750, 460))
+        pygame.display.set_caption("GameVault")
+
+        base_dir = Path(__file__).resolve().parent
+        data_dir = base_dir / "data"
+
+        self.running = True
+
+    def run(self):
+        while self.running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill((30, 30, 30))
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
