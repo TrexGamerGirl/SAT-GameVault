ac4c7bc20ffc3c632bb826d256ba29315e7ca640
TrexGamerGirl <138319@student.bmg.vic.edu.au>
2026-06-19 12:42:32 +1000

    original button system before creating a button class

--- app.py
+++ app.py
@@ -8,22 +8,47 @@ class App:
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((750, 460),pygame.RESIZABLE)
+        self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
         pygame.display.set_caption("GameVault")
+
+        self.width, self.length = self.screen.get_size()
+        print(self.width,self.length)
 
         base_dir = Path(__file__).resolve().parent
         data_dir = base_dir / "data"
+
+        font_path = base_dir / "assets" / "PixelifySans-Regular.ttf"
+        print(font_path.as_posix())
+
+        self.button_font = pygame.font.Font(font_path, 40)
 
         self.running = True
 
     def run(self):
         while self.running:
-
+            self.screen.fill((30, 30, 30))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+            
+            button_width = self.width  * 0.3
+            button_length = self.length * 0.13
+            button_rect = pygame.rect.Rect(
+                self.width // 2 - button_width //2,
+                self.length // 3- button_length //3,
+                button_width,
+                button_length
+            )
 
-            self.screen.fill((30, 30, 30))
+            pygame.draw.rect(self.screen, (255, 255, 255), button_rect)
+            text = self.button_font.render("Start", True, (0, 0, 0))
+            text_rect = text.get_rect(center=button_rect.center)
+            
+
+            self.screen.blit(text, text_rect)
+
+
+
 
             pygame.display.flip()
 
